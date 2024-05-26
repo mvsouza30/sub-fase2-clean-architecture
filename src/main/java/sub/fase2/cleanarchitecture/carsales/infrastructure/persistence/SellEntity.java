a31fd020be3435d59699c534bcbd61dc4c71ca0f
Marcos Souza <marcos-souza@outlook.com>
2024-05-26 19:14:34 -0300

    O docker-compose.yaml está rodando legal, app e db foram validados.

--- src/main/java/sub/fase2/cleanarchitecture/carsales/infrastructure/persistence/SellEntity.java
+++ src/main/java/sub/fase2/cleanarchitecture/carsales/infrastructure/persistence/SellEntity.java
@@ -3,7 +3,7 @@ package sub.fase2.cleanarchitecture.carsales.infrastructure.persistence;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
-@Table("PRE_SALES")
+@Table("pre_sales")
 public class SellEntity {
 
     @Id
